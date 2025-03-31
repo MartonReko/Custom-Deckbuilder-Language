d0935d3bf3b74574c05a3ed4271234096f900228
RekoMarton <rekomarci@gmail.com>
2025-03-31 21:38:40 +0200

    WIP working on checking stages

--- CDL/game/Stage.cs
+++ CDL/game/Stage.cs
@@ -3,9 +3,9 @@ namespace CDL.game;
 public class Stage(string name)
 {
     public readonly string Name = name;
-    public int StageLength { get; set; } = 0;
-    public int StageWidthMin { get; set; } = 0;
-    public int StageWidthMax { get; set; } = 0;
+    public int? StageLength { get; set; } 
+    public int? StageWidthMin { get; set; } 
+    public int? StageWidthMax { get; set; } 
     public List<Node> FillWith { get; set; } = [];
     public Dictionary<Node, int> MustContain { get; set; } = [];
     public Node? EndsWith { get; set; }
